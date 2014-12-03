473742e183626a326f5be43a5c16a0b587ad62c2
Christopher Bull <z3457920@student.unsw.edu.au>
2014-12-04 09:42:50 +1100

    grabdata rsync now gets some NEMO files

--- bash/grabdata_rsync.sh
+++ bash/grabdata_rsync.sh
@@ -4,13 +4,17 @@ set -x
 mkdir -p  ~/data_temp/
 
 #getting the toy files
-rsync -avz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdcore01/output/pandasHDF/toyhdf/*.h5 ~/data_temp/expt_lc_fwdcore01_toy/ 
-rsync -avz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdwesternboundary01/output/pandasHDF/toyhdf/*.h5 ~/data_temp/expt_lc_fwdwesternboundary01_toy/ 
+rsync -havz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdcore01/output/pandasHDF/toyhdf/*.h5 ~/data_temp/expt_lc_fwdcore01_toy/ 
+rsync -havz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdwesternboundary01/output/pandasHDF/toyhdf/*.h5 ~/data_temp/expt_lc_fwdwesternboundary01_toy/ 
 
 #getting a big file
-rsync -avz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdcore01/output/pandasHDF/*02*.h5 ~/data_temp/expt_lc_fwdcore01/ 
-rsync -avz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdwesternboundary01/output/pandasHDF/*02*.h5 ~/data_temp/expt_lc_fwdwesternboundary01/ 
+rsync -havz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdcore01/output/pandasHDF/*02*.h5 ~/data_temp/expt_lc_fwdcore01/ 
+rsync -havz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/expt_lc_fwdwesternboundary01/output/pandasHDF/*02*.h5 ~/data_temp/expt_lc_fwdwesternboundary01/ 
 
 #getting cookie files..
-rsync -avz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/cookiecutting07/*.p ~/data_temp/cookie_cutters/
+rsync -havz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data32/z3457920/leeuwincurrent2/cookiecutting07/*.p ~/data_temp/cookie_cutters/
+
+#getting some NEMO files..
+rsync -havz --progress z3457920@squall.ccrc.unsw.edu.au:/srv/ccrc/data23/z3457920/nemoforcingplot2/*ave* ~/data_temp/nemo_average_files/
+
 
