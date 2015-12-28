64ba350cdd3b427c31ce7ba9a7da79dd94efaaf0
Christopher Bull <z3457920@student.unsw.edu.au>
2015-12-29 07:20:43 +1100

    new snippet and a few i3 shortcuts

--- bash/yoga_fixshortcuts.sh
+++ bash/yoga_fixshortcuts.sh
@@ -8,3 +8,9 @@ openbox --reconfigure
 sudo mount -t vboxsf VM_shared_folder ~/mount_win/
 sshfs z3457920@cyclone.ccrc.unsw.edu.au:/srv/ccrc ~/ccrc -o ServerAliveInterval=60
 
+#mount home nas
+#sudo mount -t cifs //192.168.1.240/qnap01 ./mountnas  -o user=narky
+
+#actually nfs version..
+#sudo apt-get install nfs-common
+#sudo mount -t nfs 192.168.1.240:/qnap01/ /home/chris/mountnas
