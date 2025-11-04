25824ac797cc4bcc82a38b060d705d0846d30eae
Christopher Bull <5499680+chrisb13@users.noreply.github.com>
2025-11-04 15:07:17 +1100

    few more changes

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,4 +1,4 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
@@ -71,3 +71,4 @@ vim.keymap.set({"i", "s"}, "<C-E>", function()
 		ls.change_choice(1)
 	end
 end, {silent = true})
+
